8497570d983d1e73aa9ebafd504738a22f4004ba
AlessandroLimaDaSilva <coderlima@gmail.com>
2021-12-01 21:05:02 -0300

    Add loginPage.class

--- src/test/java/com/javaseleniumtemplate/pages/LoginPage.java
+++ src/test/java/com/javaseleniumtemplate/pages/LoginPage.java
@@ -5,23 +5,18 @@ import org.openqa.selenium.By;
 
 public class LoginPage extends PageBase {
     //Mapping
-    By usernameField = By.name("username");
-    By passwordField = By.name("password");
-    By loginButton = By.xpath("//input[@type='submit']");
+    By usernameField = By.xpath("//input[@id='username']");
+    By senhaField = By.xpath("//input[@id='password']");
+    By entrarButton = By.xpath("//input[@value='Entrar']");
     By mensagemErroTextArea = By.xpath("/html/body/div[2]/font");
 
-    //Actions
-    public void preenhcerUsuario(String usuario){
-        sendKeys(usernameField, usuario);
-    }
 
-    public void preencherSenha(String senha){
-        sendKeys(passwordField, senha);
-    }
+    public void preencherUsuario(String usuario){ sendKeys(usernameField, usuario);}
 
-    public void clicarEmLogin(){
-        click(loginButton);
-    }
+    public void clicarEmEntrar(){ click(entrarButton);}
+
+    public void preencherSenha(String senha){ sendKeys(senhaField, senha);}
+
 
     public String retornaMensagemDeErro(){
         return getText(mensagemErroTextArea);
